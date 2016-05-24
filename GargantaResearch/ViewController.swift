1b42e025b083d525a26eab214b6de7c964641301
gustavochx <gustavochx@gmail.com>
2016-05-24 14:53:52 -0300

    first

---
+++ GargantaResearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GargantaResearch
+//
+//  Created by Gustavo Henrique on 24/05/16.
+//  Copyright © 2016 Gustavo Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
