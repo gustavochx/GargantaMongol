28f623eccc7fc27980403e57cbc5d29f1d6c377d
Yago Teixeira <yagoteixeira@MacBook-Pro-de-Yago.local>
2016-05-25 16:25:05 -0300

    Password

---
+++ GargantaResearch/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  GargantaResearch
+//
+//  Created by Yago Teixeira on 25/05/16.
+//  Copyright © 2016 Gustavo Henrique. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GargantaResearch/PesquisaSegue.swift
@@ -0,0 +1,9 @@
+//
+//  PesquisaSegue.swift
+//  GargantaResearch
+//
+//  Created by Yago Teixeira on 25/05/16.
+//  Copyright © 2016 Gustavo Henrique. All rights reserved.
+//
+
+import Foundation
